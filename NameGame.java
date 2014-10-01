145ec0be33b5730b02b916e586dbe7732ce98062
Felicia <lrgrable@ls-macbook-air.home>
2014-10-01 21:25:29 +0100

    Removed TODO's

--- NameGame.java
+++ NameGame.java
@@ -65,9 +65,7 @@ public class NameGame {
 
 		  }
 		}
-		// TODO Produce the required four lines of verse 
-		// TODO Print the verse to standard output
-		// TODO remove "to do" comments when they are "to done"
+		
 	}
 	
 	/*
