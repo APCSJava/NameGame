c9de7202c621175431cd61e449f692f56aa350bc
Rohan Prasad <rohanprasad@Rohans-MacBook-Air.local>
2014-10-02 12:47:28 +0100

    Made NameGame file

--- NameGame.java
+++ NameGame.java
@@ -13,15 +13,46 @@
  * a person's name begins with one of the target substitutions (b, f, or m).
  * 
  * @author Kent Collins
- * @author <additional author(s); if multiple, separate with commas>
- * @version 30 September 2014
+ * @author Rohan Prasad, Tristan Burke.
+ * @version 2014, October 2
  */
 
 import java.util.Scanner;
 
 public class NameGame {
 	public static void main(String[] args) {
-		// TODO Read the name from standard input 
+		// TODO Read the name from standard input
+		Scanner scnr = new Scanner(System.in);
+		System.out.println("Please enter a name to Name-Game-ify:");
+		String name = scnr.next(); 
+		if (name.substring(0,findFirstVowel(name)).equals("b")) {
+			String noConsonant = name.substring(findFirstVowel(name),name.length());
+			System.out.println(name + ", " + name + ", " + "bo-" + noConsonant);
+			System.out.println("Banana-fana fo-f" + noConsonant);
+			System.out.println("Fee-fi-mo-m" + noConsonant);
+			System.out.println(name + "!");
+		}
+		else if (name.substring(0,findFirstVowel(name)).equals("m")) {
+                        String noConsonant = name.substring(findFirstVowel(name),name.length());
+                        System.out.println(name + ", " + name + ", " + "bo-b" + noConsonant);
+                        System.out.println("Banana-fana fo-f" + noConsonant);
+                        System.out.println("Fee-fi-mo-" + noConsonant);
+                        System.out.println(name + "!");
+                }
+		else if (name.substring(0,findFirstVowel(name)).equals("f") || name.substring(0,findFirstVowel(name)).equals("ph")) {
+                        String noConsonant = name.substring(findFirstVowel(name),name.length());
+                        System.out.println(name + ", " + name + ", " + "bo-b" + noConsonant);
+                        System.out.println("Banana-fana fo-" + noConsonant);
+                        System.out.println("Fee-fi-mo-m" + noConsonant);
+                        System.out.println(name + "!");
+                }
+		else {
+                        String noConsonant = name.substring(findFirstVowel(name),name.length());
+                        System.out.println(name + ", " + name + ", " + "bo-b" + noConsonant);
+                        System.out.println("Banana-fana fo-f" + noConsonant);
+                        System.out.println("Fee-fi-mo-m" + noConsonant);
+                        System.out.println(name + "!");
+                }
 		// TODO Produce the required four lines of verse 
 		// TODO Print the verse to standard output
 		// TODO remove "to do" comments when they are "to done"
@@ -34,6 +65,7 @@ public class NameGame {
 	 * @return 	The position (index) of the first vowel or -1 if no vowels present.
 	 */
 	public static int findFirstVowel(String s) {
+		s = s.toLowerCase();
 		for (int i = 0; i<s.length(); i++) {
 			char letter = s.charAt(i);
 			if (letter=='a' || letter=='e' || letter=='i' || letter =='o' || letter =='u') {
