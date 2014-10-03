78f74607e1a6af66e4e3e42e05958302f59e6dfe
amnaha15 <amnaha15@asl.org>
2014-10-03 14:27:46 +0100

    Update NameGame.java
    Wrote NameGame - works for all names I tested  so far

--- NameGame.java
+++ NameGame.java
@@ -13,7 +13,7 @@
  * a person's name begins with one of the target substitutions (b, f, or m).
  * 
  * @author Kent Collins
- * @author <additional author(s); if multiple, separate with commas>
+ * @author <additional author: Amnah Ahmad>
  * @version 30 September 2014
  */
 
@@ -21,22 +21,56 @@ import java.util.Scanner;
 
 public class NameGame {
 	public static void main(String[] args) {
-		// TODO Read the name from standard input 
-		// TODO Produce the required four lines of verse 
-		// TODO Print the verse to standard output
-		// TODO remove "to do" comments when they are "to done"
+	Scanner scan = new Scanner(System.in);
+	
+	System.out.print("Enter your name");
+	String name = scan.nextLine();
+	int vow = findFirstVowel(name);
+	String back = name.substring(vow, name.length());
+
+	if(vow == 0){
+	String nameL = name.toLowerCase();
+	System.out.println(name + ", " + name + ", " + "bo-b" + nameL);
+	System.out.println("Banana-fana fo-f" + nameL);
+	System.out.println("Fee-fi-mo-m" + nameL);
+	System.out.println(name+ "!");
+	}
+	else if(name.charAt(0)== 'B' || name.charAt(0) == 'b'){
+	System.out.println( name + ", " +  name + ", " + "bo-" + back);
+	System.out.println( "Banana-fana fo-f" + back);
+	System.out.println( "Fee-fi-mo-m" + back);
+	System.out.println( name + "!");
+	}
+	else if(name.charAt(0)== 'M' || name.charAt(0) == 'm'){
+	System.out.println( name + ", " +  name + ", " + "bo-b" + back);
+	System.out.println( "Banana-fana fo-f" + back);
+	System.out.println( "Fee-fi-mo-" + back);
+	System.out.println( name + "!");
+	}
+	else if(name.charAt(0)== 'F' || name.charAt(0) == 'f'){
+	System.out.println( name + ", " +  name + ", " + "bo-b" + back);
+	System.out.println( "Banana-fana fo-" + back);
+	System.out.println( "Fee-fi-mo-m" + back);
+	System.out.println( name + "!");
+	}
+	else{
+	System.out.println( name + ", " +  name + ", " + "bo-b" + back);
+	System.out.println( "Banana-fana fo-f" + back);
+	System.out.println( "Fee-fi-mo-m" + back);
+	System.out.println( name + "!");
+	}
 	}
 	
 	/*
 	 * Locates the position of the first occurrence of a vowel
-	 * 
 	 * @param s The string to be analyzed.  
 	 * @return 	The position (index) of the first vowel or -1 if no vowels present.
 	 */
 	public static int findFirstVowel(String s) {
-		for (int i = 0; i<s.length(); i++) {
-			char letter = s.charAt(i);
-			if (letter=='a' || letter=='e' || letter=='i' || letter =='o' || letter =='u') {
+	for (int i = 0; i<s.length(); i++) {
+		s= s.toLowerCase();
+		char letter = s.charAt(i);
+	if (letter=='a' || letter=='e' || letter=='i' || letter =='o' || letter =='u') {
 				return i;
 			}
 		
@@ -44,4 +78,5 @@ public class NameGame {
 		
 	}
 }
+
 
